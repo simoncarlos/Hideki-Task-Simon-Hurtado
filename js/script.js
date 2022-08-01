9d9471ac5fbdb88458ad4b70e040b35be8d08565
Carlos Diego Simon Hurtado <diegoestushoi@gmail.com>
2022-08-01 14:33:36 -0300

    Subida Primera Pre-Entrega del proyecto final

---
+++ js/script.js
@@ -0,0 +1,147 @@
+const arrayTareas = [];
+
+class Tarea{
+    constructor(id, nombre, descripcion, prioridad){
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.prioridad = prioridad;
+    }
+    mostrar() {
+        alert("\n\nEl id de tarea es: " + this.id + "\n\nEl nombre de la tarea es: " + this.nombre + "\n\nTu tarea es: " + this.descripcion + "\n\nSu prioridad es: " + this.prioridad);
+    }
+}
+
+function modificarTarea(){
+
+    let nombreBusqueda = prompt("Ingrese el nombre de la tarea que desea modificar")
+
+    let posicion = arrayTareas.findIndex( element => element.nombre == nombreBusqueda );
+
+    if( posicion != -1 ){
+
+        arrayTareas[posicion].nombre = prompt("Ahora ingrese nuevo nombre de la tarea");
+        arrayTareas[posicion].descripcion = prompt("Ahora ingrese la nueva descripcion");
+        alert("A continuacion le mostramos la lista de tareas por pantalla");
+        arrayTareas.forEach( element => { element.mostrar() } );
+
+    }
+    else{
+        alert("Su nombre de tarea ingresado no existe :(");
+    }
+
+}
+
+function agregarTarea(id){
+
+    arrayTareas.unshift(new Tarea( // Agregar tarea
+        id , // ID de la tarea
+        prompt("Por favor ingrese el nombre de la tarea que debe realizar"), // Nombre de la tarea
+        prompt("Por favor ingrese la descripción de la tarea que debe realizar"),  // Descripcion de la tarea
+        prompt("Por favor ingrese la prioridad que va a tener su proyecto") // Prioridad de la tarea
+    ));
+
+}
+
+function borrarTarea(){
+
+    let idBusqueda = prompt("Ingrese el id de la tarea que desea borrar:")
+
+    let indice = arrayTareas.findIndex( element => element.id == idBusqueda );
+
+    if( indice != -1 ){
+
+        alert("¡Su tarea " + arrayTareas[indice].nombre + " fue borrado con exito!");// Corregir problema 
+        arrayTareas.splice((indice), 1);
+
+        if( arrayTareas.length != 0 ){
+            alert("A continuacion le mostramos la lista de tareas por pantalla");//modificar esto
+            arrayTareas.forEach( element => { element.mostrar() } );
+        }
+        else{
+            alert("No existen tareas para mostrar, debe ingresar una nueva tarea");
+        }
+
+    }
+    else{
+        alert("Su id de tarea ingresado no existe :(");
+    }
+
+}
+
+function alerta( mensaje ){
+    alert(mensaje);
+}
+
+function condicionalCantidad( funcion1, funcion2 ){
+
+    if( arrayTareas.length > 0 ){
+        funcion1();
+    }
+    else{
+        funcion2();
+    }
+
+}
+
+let nombre = prompt("¡Hola! Por favor ingrese su nombre a continuacion..");
+let salir = false;
+
+do{
+
+    //let opcion = prompt("Hola " + nombre + ",¿Que desea hacer? \n1 - Ver Lista de tareas \n2 - Agregar una tarea \n3 - Modificar una tarea  \n4 - Eliminar una tarea \n5 - Salir");
+
+    switch( prompt("Hola " + nombre + ",¿Que desea hacer? \n1 - Ver Lista de tareas \n2 - Agregar una tarea \n3 - Modificar una tarea  \n4 - Eliminar una tarea \n5 - Salir") ){
+        case "1" :
+            //if( arrayTareas.length != 0 ){
+            //    arrayTareas.forEach( element => { element.mostrar() } );
+            //}
+            //else{
+            //    alert("No existen tareas para mostrar, debe ingresar una nueva tarea");
+            //}
+            condicionalCantidad( () => { arrayTareas.forEach( element => { element.mostrar() } ) } , () => { alert("No existen tareas para mostrar, debe ingresar una nueva tarea")} );
+            break;
+    
+        case "2" :
+
+            //if( arrayTareas.length != 0 ){
+            //    agregarTarea(  arrayTareas[0].id + 1  );
+            //}
+            //else{
+            //    agregarTarea( 1 );
+            //}
+            condicionalCantidad( () => { agregarTarea( arrayTareas[0].id + 1 ) } , () => { agregarTarea( 1 ) } );
+            break;
+        
+        case "3" :
+
+            //if( arrayTareas.length != 0 ){
+            //    modificarTarea();
+            //}
+            //else{
+            //    alert("No existen tareas para modificar, debe ingresar una nueva tarea");
+            //}
+            condicionalCantidad( () => { modificarTarea() } , () => { alert("No existen tareas para modificar, debe ingresar una nueva tarea") } );
+            break;
+
+        case "4" :
+
+            //if( arrayTareas.length != 0 ){
+            //    borrarTarea();
+            //}
+            //else{
+            //    alert("No existen tareas para borrar, debe ingresar una nueva tarea");
+            //}
+            condicionalCantidad( () => { borrarTarea() } , () => { alert("No existen tareas para borrar, debe ingresar una nueva tarea") } );
+            break;
+    
+        case "5" :
+            salir = true;
+            break;
+    
+        default:
+            alert(nombre + ", la opcion ingresada no es valida, por favor reintente");
+            break;    
+    }
+
+}while(!salir);
