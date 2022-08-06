08835fe2994d77c27a6a0c3b7f7038c48fb72703
Carlos Diego Simon Hurtado <diegoestushoi@gmail.com>
2022-08-05 23:04:30 -0300

    Arreglos prototipo funcional ABM - DOM y Eventos

--- js/script.js
+++ js/script.js
@@ -12,14 +12,6 @@ class Tarea{
         this.prioridad = prioridad;
         this.descripcion = descripcion;
     }
-    mostrar() {
-        alert("\n\nEl id de tarea es: " + this.id + "\n\nEl nombre de la tarea es: " + this.nombre + "\n\nTu tarea es: " + this.descripcion + "\n\nSu prioridad es: " + this.prioridad);
-    }
-    modificar(nombre, descripcion, prioridad) {
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.prioridad = prioridad;
-    }
 }
 
 class Usuario{
@@ -29,46 +21,42 @@ class Usuario{
         this.contraseña = contraseña;
         this.rol = rol;
         this.fotoSrc= fotoSrc;
-        this.tareas = tareas;//array
+        this.tareas = tareas;  //array de tareas asignadas al usuario
     }
 }
 
 function crearTareas(){
-    let boton = document.querySelector(".bxs-message-square-add");
-    boton.addEventListener("click", () =>{
-
-        let nuevoId = arrayTareas[0].id + 1;
-        arrayTareas.unshift(new Tarea( nuevoId , " ", "Terminado" ,   [ 1 , 2 , 3 ] , "6 ene" ,  "19 ene" ,  "Medio" , " " ));
+    document.querySelector(".bxs-message-square-add").addEventListener("click", () =>{
+        let nuevoId = 1;
+        if(arrayTareas.length != 0){
+            nuevoId = arrayTareas[0].id + 1;
+        }
+        arrayTareas.unshift(new Tarea( nuevoId , " ", "Terminado" ,   [ 1 , 2 , 3 ] , "6 ene" ,  "19 ene" ,  "Medio" , "Aprobar entrega de interactuar con HTML de javascript" ));
         mostrarTareas();
-        let click = document.getElementById(nuevoId);
-        click.click();
-
+        document.getElementById(nuevoId).click(); //Se realiza click en el boton apenas creado el evento para modificarlo 
     });
 }
 
 function mostrarTareas(){
-    console.log(arrayTareas);
     let listaTareas = document.querySelector(".tarea__lista");
     listaTareas.innerHTML = " ";
     arrayTareas.forEach( element => {
         
         let colorPrioridad = "bajo";
         let colorEstado = "proceso";
-    
+        // Se asignan a la variables colorPrioridad y colorEstado para luego insertarlas en el HTML como clases, segun su prioridad o estado
         if( element.prioridad == "Alto" ){
             colorPrioridad = "alto";
-        }
-        else if( element.prioridad == "Medio" ){
+        }else if( element.prioridad == "Medio" ){
             colorPrioridad = "medio";
         }
     
         if( element.estado == "Terminado" ){
             colorEstado = "terminado";
-        }
-        else if( element.estado == "Comenzar" ){
+        }else if( element.estado == "Comenzar" ){
             colorEstado = "hacer";
         }
-    
+        // Datos de cada tarea
         listaTareas.innerHTML += `  <div class="tarea__item">
                                         <div class="tarea__nombre">${element.nombre}</div>
                                         <div class="tarea__estado">
@@ -89,13 +77,14 @@ function mostrarTareas(){
                                         <div class="tarea__opciones"> <i id="${element.id}" class='bx bx-edit' data-bs-toggle="modal" data-bs-target="#exampleModal"></i><i id="borrar${element.id}" class='bx bx-trash-alt'></i> </div>
                                     </div>`;
 
-        let idTareaColaboradores = "tarea"+element.id;
-        let listaColaboradores = document.getElementById(idTareaColaboradores);
-
         element.colaboradores.forEach( idColaborador => {
-            listaColaboradores.innerHTML += `   <div>
-                                                    <img src="assets/profiles/${idColaborador}.png">
-                                                </div>`;
+            arrayUsuarios.forEach( usuario =>{
+                if( usuario.id == idColaborador ){ 
+                    document.getElementById("tarea"+element.id).innerHTML += `   <div>
+                                                                                    <img src="assets/profiles/${usuario.fotoSrc}">
+                                                                                </div>`;
+                }
+            });
         });
     });
 
@@ -107,14 +96,14 @@ function modificarTareas(){
 
     let lista = document.querySelectorAll('.bx-edit');
     let modificar = document.querySelector(".tarea__popup");
-
+    //Una vez guardados en el array "lista", se recorre cada elemento con .bx-edit para añadirle el evento click
     lista.forEach(element => {
         element.addEventListener('click', (e) =>{ 
-
+            //se busca la posicion de la tarea que tenga un id coincidente con el id del .bx-edit para insertar las propiedades del objeto.
             let indicePosicion = arrayTareas.findIndex( position => { 
                 return position.id == e.target.id 
             });
-            modificar.innerHTML = " ";//${arrayTareas[ (arrayTareas.length - e.target.id) ].nombre}
+            modificar.innerHTML = " ";
             modificar.innerHTML += `<div class="tarea__modificar">  
                                         <i class='bx bx-x-circle' data-bs-dismiss="modal"></i>
                                         <label id="placeholder">Nombre de la tarea</label>
@@ -128,7 +117,7 @@ function modificarTareas(){
                                             <input type="radio" name="estado" id="enProceso">
                                             <label for="enProceso">En Proceso</label>
 
-                                            <input type="radio" name="estado" id="comenzar">
+                                            <input type="radio" name="estado" checked id="comenzar">
                                             <label for="comenzar">Comenzar</label>
                                         </div>
 
@@ -143,7 +132,7 @@ function modificarTareas(){
                                             <input type="radio" name="prioridad" id="media">
                                             <label for="media">Medio</label>
 
-                                            <input type="radio" name="prioridad" id="baja">
+                                            <input type="radio" name="prioridad" checked id="baja">
                                             <label for="baja">Bajo</label>
                                         </div>
                                         <div class="tarea__participantes">
@@ -151,79 +140,34 @@ function modificarTareas(){
                                             <ul class="tarea__modificar--profiles">
                                             </ul>
                                         </div>
-                                    </div>`;
-            //modificar.style.display = "block";
-            //let botonCerrar = document.querySelector(".bx-x-circle");
-            //botonCerrar.addEventListener("click", () => {
-            //    modificar.style.display = "none";
-            //});
-
-            let listaColaboradores = document.querySelector(".tarea__modificar--profiles");
-
+                                    </div>`;            
+            // Se recorren los ID de los colaboradores que se encuentra dentro de la propiedad "colaboradores" del cada tarea para luego comparar con los id de la lista de usuarios y devolver su posicion dentro del array de usuarios
             arrayTareas[ indicePosicion ].colaboradores.forEach( element => {
                 let posicion = arrayUsuarios.findIndex( usuario => usuario.id == element );
-                listaColaboradores.innerHTML += `
+                document.querySelector(".tarea__modificar--profiles").innerHTML += `
                                                 <li id="${element}">
-                                                    <img src="assets/profiles/${element}.png">
+                                                    <img src="assets/profiles/${arrayUsuarios[posicion].fotoSrc}">
                                                     <article>
                                                         <p class="tarea__perfil--nombre">${arrayUsuarios[posicion].nombre}</p>
                                                         <p class="tarea__perfil--rol">${arrayUsuarios[posicion].rol}</p>
                                                     </article>
                                                 </li>`; 
             });
-            
+            // Segun el valor del estado de la tarea, se va a mostrar como checked el que coincida con el del array de las tareas
             if( arrayTareas[ indicePosicion ].estado == "Terminado" ){
-                let estado1 = document.getElementById("terminado");
-                let estado2 = document.getElementById("enProceso");
-                let estado3 = document.getElementById("comenzar");
-                estado1.checked = true;
-                estado2.checked = false;
-                estado3.checked = false;
+                document.getElementById("terminado").checked = true;
+            }else if( arrayTareas[ indicePosicion ].estado == "En proceso" ){
+                document.getElementById("enProceso").checked = true;
             }
-            else if( arrayTareas[ indicePosicion ].estado == "En proceso" ){
-                let estado1 = document.getElementById("terminado");
-                let estado2 = document.getElementById("enProceso");
-                let estado3 = document.getElementById("comenzar");
-                estado1.checked = false;
-                estado2.checked = true;
-                estado3.checked = false;
-            }else{
-                let estado1 = document.getElementById("terminado");
-                let estado2 = document.getElementById("enProceso");
-                let estado3 = document.getElementById("comenzar");
-                estado1.checked = false;
-                estado2.checked = false;
-                estado3.checked = true;
-            }
-
+            // Segun el valor de la prioridad de la tarea, se va a mostrar como checked el que coincida con el del array de las tareas
             if( arrayTareas[ indicePosicion ].prioridad == "Alto" ){
-                let prioridad1 = document.getElementById("alta");
-                let prioridad2 = document.getElementById("media");
-                let prioridad3 = document.getElementById("baja");
-                prioridad1.checked = true;
-                prioridad2.checked = false;
-                prioridad3.checked = false;
+                document.getElementById("alta").checked = true;
+            }else if( arrayTareas[ indicePosicion ].prioridad == "Medio" ){
+                document.getElementById("media").checked = true;
             }
-            else if( arrayTareas[ indicePosicion ].prioridad == "Medio" ){
-                let prioridad1 = document.getElementById("alta");;
-                let prioridad2 = document.getElementById("media");;
-                let prioridad3 = document.getElementById("baja");
-                prioridad1.checked = false;
-                prioridad2.checked = true;
-                prioridad3.checked = false;
-            }else{
-                let prioridad1 = document.getElementById("alta");
-                let prioridad2 = document.getElementById("media");
-                let prioridad3 = document.getElementById("baja");
-                prioridad1.checked = false;
-                prioridad2.checked = false;
-                prioridad3.checked = true;
-            }
-
+            
             //Escucha de eventos
-
-            let name = document.querySelector(".tarea__modificar--nombre"); //Escucha del nombre
-
+            let name = document.querySelector(".tarea__modificar--nombre"); 
             name.addEventListener("change", () => {
                 arrayTareas[ indicePosicion ].nombre = name.value;
                 mostrarTareas();
@@ -235,8 +179,7 @@ function modificarTareas(){
                 mostrarTareas();
             });
 
-            var states = document.querySelectorAll('input[type=radio][name="estado"]');
-            states.forEach( option => { 
+            document.querySelectorAll('input[type=radio][name="estado"]').forEach( option => { 
                 option.addEventListener('change', () => {
                     if( option.id == "terminado" ){
                         arrayTareas[ indicePosicion ].estado = "Terminado";
@@ -251,8 +194,7 @@ function modificarTareas(){
                 }); 
             });
 
-            var priority = document.querySelectorAll('input[type=radio][name="prioridad"]');
-            priority.forEach( option => { 
+            document.querySelectorAll('input[type=radio][name="prioridad"]').forEach( option => { 
                 option.addEventListener('change', () => {
                     if( option.id == "alta" ){
                         arrayTareas[indicePosicion].prioridad = "Alto";
@@ -266,14 +208,14 @@ function modificarTareas(){
                     mostrarTareas(); 
                 }); 
             });
+
         });
     });
 }
 
 function eliminarTareas(){
-
-    let botonEliminar = document.querySelectorAll(".bx-trash-alt");
-    botonEliminar.forEach( element => {
+    // Se añade el evento de click a cada boton de borrar para eliminar del arrayTareas el elemento cuya posicion coincida con el id de la etiqueta que contiene la clase .bx-trash-alt
+    document.querySelectorAll(".bx-trash-alt").forEach( element => {
         element.addEventListener("click", (e) =>{
             let indice = arrayTareas.findIndex( element2 => { 
                 let idComparacion = "borrar"+element2.id;
@@ -283,17 +225,17 @@ function eliminarTareas(){
             mostrarTareas();
         });
     });
+
 }
 
-//Entrega de DOM
-arrayTareas.unshift(new Tarea( 1 , "Responsive Design", "Terminado" ,   [ 1 , 2 , 3 ] , "6 ene" ,    "19 ene" ,  "Medio" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 2 , "Web Development",   "En proceso" ,  [ 2 , 3 , 4 ] , "17 ene" ,   "13 feb" ,  "Alto" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 3 , "Databases",         "Comenzar" ,    [ 3 , 4 , 5 ] , "23 feb" ,   "1 mar" ,   "Bajo" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 4 , "Final Proyect",     "En proceso" ,  [ 4 , 5 , 6 ] , "9 may" ,    "19 may" ,  "Medio" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 5 , "Wireframes",        "Terminado" ,   [ 5 , 6 , 1 ] , "30 may" ,   "5 jun" ,   "Medio" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 6 , "Entrevistas",       "En proceso" ,  [ 6 , 1 , 2 ] , "27 jun" ,   "5 jul" ,   "Alto" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 7 , "Benchmarking",      "Comenzar" ,    [ 1 , 2 , 3 ] , "1 jul" ,    "3 agos" ,  "Bajo" , "Hacer diseño responsivo desitio web xdxdxd" ));
-arrayTareas.unshift(new Tarea( 8 , "POV + MVP",         "En proceso" ,  [ 2 , 3 , 4 ] , "23 agos" ,  "23 sep" ,  "Bajo" , "Hacer diseño responsivo desitio web xdxdxd" ));
+arrayTareas.unshift(new Tarea( 1 , "Responsive Design", "Terminado" ,   [ 1 , 2 , 3 , 6] ,  "6 ene" ,    "19 ene" ,  "Medio" , "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 2 , "Web Development",   "En proceso" ,  [ 2 , 3 , 4 ] ,     "17 ene" ,   "13 feb" ,  "Alto" ,  "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 3 , "Databases",         "Comenzar" ,    [ 3 , 5 ] ,         "23 feb" ,   "1 mar" ,   "Bajo" ,  "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 4 , "Final Proyect",     "En proceso" ,  [ 4 , 5 , 6 ] ,     "9 may" ,    "19 may" ,  "Medio" , "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 5 , "Wireframes",        "Terminado" ,   [ 5 ] ,             "30 may" ,   "5 jun" ,   "Medio" , "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 6 , "Entrevistas",       "En proceso" ,  [ 3 , 2 , 5 ] ,     "27 jun" ,   "5 jul" ,   "Alto" ,  "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 7 , "Benchmarking",      "Comenzar" ,    [ 1 , 2 , 3 , 5] ,  "1 jul" ,    "3 agos" ,  "Bajo" ,  "Aprobar entrega de interactuar con HTML de javascript" ));
+arrayTareas.unshift(new Tarea( 8 , "POV + MVP",         "En proceso" ,  [ 3 , 4 ] ,         "23 agos" ,  "23 sep" ,  "Bajo" ,  "Aprobar entrega de interactuar con HTML de javascript" ));
 
 arrayUsuarios.unshift(new Usuario( 1 , "Solange",   "contraseña" , "Tutora" ,     "1.png" ,  ["1", "2", "3"]  ));
 arrayUsuarios.unshift(new Usuario( 2 , "Florencia", "contraseña" , "Profesora",   "18.png" , ["2", "3", "4"]  ));
@@ -302,10 +244,5 @@ arrayUsuarios.unshift(new Usuario( 4 , "Diana",     "contraseña" , "Tutora" ,  
 arrayUsuarios.unshift(new Usuario( 5 , "Mariano",   "contraseña" , "Coordinador", "13.png" , ["5", "6", "7"]  ));
 arrayUsuarios.unshift(new Usuario( 6 , "Diego",     "contraseña" , "Alumno" ,     "7.png" ,  ["6", "7", "8"]  ));
 
-console.log(arrayUsuarios);
-
 crearTareas();
 mostrarTareas();
-
-
-
